481db03153da7dcc1af8f1a7d6aea9994286ee22
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2023-06-26 13:51:57 +0100

    Add 'number' method to PullRequest class
    We'll want to expose the PR number in our logging, in a subsequent
    commit.

--- lib/pull_request.rb
+++ lib/pull_request.rb
@@ -5,6 +5,10 @@ class PullRequest
     @api_response = api_response
   end
 
+  def number
+    @api_response.number
+  end
+
   def merge!
     puts "Merging #{@api_response.base.repo.name}##{@api_response.number}..."
     GitHubClient.instance.merge_pull_request("alphagov/#{@api_response.base.repo.name}", @api_response.number)
