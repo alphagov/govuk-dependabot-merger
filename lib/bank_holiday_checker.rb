04ddf40cadad1b4a7caa9db4458682ed9dddae92
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2023-06-26 12:52:44 +0100

    Add `BankHolidayChecker` to determine if today is a bank holiday
    We'll use this class in the next commit to constrain the auto
    merger to prevent it from merging otherwise satisfactory PRs on
    bank holidays. This is in case something goes wrong, and there
    are no engineers around to step in.

---
+++ lib/bank_holiday_checker.rb
@@ -0,0 +1,8 @@
+class BankHolidayChecker
+  def self.is_bank_holiday?
+    uri = "https://www.gov.uk/bank-holidays.json"
+    bank_holidays_data = JSON.parse(Net::HTTP.get(URI.parse(uri)))
+    dates = bank_holidays_data["england-and-wales"]["events"].map { |e| e["date"] }
+    dates.include? Time.now.strftime("%Y-%m-%d")
+  end
+end
---
+++ spec/lib/bank_holiday_checker_spec.rb
@@ -0,0 +1,41 @@
+require "timecop"
+require_relative "../../lib/bank_holiday_checker"
+
+RSpec.describe BankHolidayChecker do
+  before do
+    example_bank_holiday_data = {
+      "england-and-wales": {
+        "division": "england-and-wales",
+        "events": [
+          {
+            "title": "New Year’s Day",
+            "date": "2018-01-01",
+            "notes": "",
+            "bunting": true,
+          },
+          {
+            "title": "Good Friday",
+            "date": "2018-03-30",
+            "notes": "",
+            "bunting": false,
+          },
+        ],
+      },
+    }
+    stub_request(:get, "https://www.gov.uk/bank-holidays.json")
+      .to_return(status: 200, body: example_bank_holiday_data.to_json)
+  end
+
+  describe "#is_bank_holiday?" do
+    it "should return true if today is a bank holiday" do
+      bank_holiday_date = Time.local(2018, 0o3, 30, 10, 5, 0)
+      Timecop.freeze(bank_holiday_date) do
+        expect(BankHolidayChecker.is_bank_holiday?).to eq(true)
+      end
+    end
+
+    it "should return false if today is not a bank holiday" do
+      expect(BankHolidayChecker.is_bank_holiday?).to eq(false)
+    end
+  end
+end
