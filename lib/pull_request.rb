7b50e115bf2b67a6aff6cb133d62072a847db8bd
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2023-06-26 13:58:38 +0100

    Add stub `is_auto_mergeable?` method
    This method will invoke some PR-specific checks as per RFC-156.

--- lib/pull_request.rb
+++ lib/pull_request.rb
@@ -9,6 +9,11 @@ class PullRequest
     @api_response.number
   end
 
+  def is_auto_mergeable?
+    # TODO
+    true
+  end
+
   def merge!
     puts "Merging #{@api_response.base.repo.name}##{@api_response.number}..."
     GitHubClient.instance.merge_pull_request("alphagov/#{@api_response.base.repo.name}", @api_response.number)
