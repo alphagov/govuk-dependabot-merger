9446354457d2600cef575a9cf155e3640dc84a33
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2023-06-27 17:09:24 +0100

    Add validation that remote config file exists and is versioned
    Repos will interface with this automerge repo via a config file
    at the root, called `.govuk_automerge_config.yml`. This file will
    define which dependencies can be auto-merged. For now, the
    expectation is that its contents will look something like this:
    
    ```
    dependabot_auto_merge_config:
      api_version: 0 # we'll change to 1 when the repo is ready to use
      auto_merge:
        - dependency: govuk_publishing_components
          allowed_semver_bumps:
            - patch
            - minor
        - dependency: rubocop-govuk
          allowed_semver_bumps:
            - patch
            - minor
    ```
    
    This file has been added to one repo ready:
    https://github.com/alphagov/govuk-developer-docs/blob/8b4d11543f24fcac16217ec533003178c8a68513/.govuk_automerge_config.yml

--- lib/pull_request.rb
+++ lib/pull_request.rb
@@ -1,4 +1,6 @@
+require "yaml"
 require_relative "./github_client"
+require_relative "./version"
 
 class PullRequest
   attr_reader :reasons_not_to_merge
@@ -17,6 +19,8 @@ class PullRequest
       reasons_not_to_merge << "PR contains more than one commit."
     elsif !validate_files_changed
       reasons_not_to_merge << "PR changes files that should not be changed."
+    elsif !validate_external_config_file
+      reasons_not_to_merge << "The remote .govuk_automerge_config.yml file is missing or in the wrong format."
     end
 
     reasons_not_to_merge.count.zero?
@@ -34,8 +38,28 @@ class PullRequest
     files_changed == ["Gemfile.lock"]
   end
 
+  def validate_external_config_file
+    return false unless remote_config.dig("dependabot_auto_merge_config", "api_version") == DependabotAutoMerge::VERSION
+
+    true
+  end
+
   def merge!
     puts "Merging #{@api_response.base.repo.name}##{@api_response.number}..."
     GitHubClient.instance.merge_pull_request("alphagov/#{@api_response.base.repo.name}", @api_response.number)
+  end
+
+private
+
+  def remote_config
+    @remote_config ||= YAML.load(GitHubClient.instance.contents(
+                                   "alphagov/#{@api_response.base.repo.name}",
+                                   {
+                                     accept: "application/vnd.github.raw",
+                                     path: ".govuk_automerge_config.yml",
+                                   },
+                                 ))
+  rescue Octokit::NotFound
+    {}
   end
 end
--- spec/lib/pull_request_spec.rb
+++ spec/lib/pull_request_spec.rb
@@ -68,6 +68,7 @@ RSpec.describe PullRequest do
       ],
     }
   end
+  let(:external_config_file_api_url) { "https://api.github.com/repos/alphagov/#{repo_name}/contents/.govuk_automerge_config.yml" }
 
   describe ".initialize" do
     it "should take a GitHub API response shaped pull request" do
@@ -103,10 +104,21 @@ RSpec.describe PullRequest do
       ])
     end
 
+    it "should make a call to validate_external_config_file" do
+      pr = create_pull_request_instance
+      allow(pr).to receive(:validate_external_config_file).and_return(false)
+      expect(pr).to receive(:validate_external_config_file)
+      pr.is_auto_mergeable?
+      expect(pr.reasons_not_to_merge).to eq([
+        "The remote .govuk_automerge_config.yml file is missing or in the wrong format.",
+      ])
+    end
+
     def create_pull_request_instance
       pr = PullRequest.new(pull_request_api_response)
       allow(pr).to receive(:validate_single_commit).and_return(true)
       allow(pr).to receive(:validate_files_changed).and_return(true)
+      allow(pr).to receive(:validate_external_config_file).and_return(true)
       pr
     end
   end
@@ -158,6 +170,37 @@ RSpec.describe PullRequest do
     end
   end
 
+  describe ".validate_external_config_file" do
+    it "returns false if there is no automerge config file in the repo" do
+      stub_request(:get, external_config_file_api_url)
+        .to_return(status: 404)
+
+      pr = PullRequest.new(pull_request_api_response)
+      expect(pr.validate_external_config_file).to eq(false)
+    end
+
+    it "returns false if the automerge config file is on a different version" do
+      contents_api_response = <<~EXTERNAL_CONFIG_YAML
+        dependabot_auto_merge_config:
+          api_version: -1
+          foo: bar
+      EXTERNAL_CONFIG_YAML
+
+      stub_request(:get, external_config_file_api_url)
+        .to_return(status: 200, body: contents_api_response.to_json, headers: { "Content-Type": "application/json" })
+
+      pr = PullRequest.new(pull_request_api_response)
+      expect(pr.validate_external_config_file).to eq(false)
+    end
+
+    it "returns true if the automerge config file exists and contains nothing unexpected" do
+      stub_remote_allowlist
+
+      pr = PullRequest.new(pull_request_api_response)
+      expect(pr.validate_external_config_file).to eq(true)
+    end
+  end
+
   describe ".merge!" do
     it "should output the name and PR number" do
       pr = PullRequest.new(pull_request_api_response)
@@ -172,4 +215,23 @@ RSpec.describe PullRequest do
     stub_request(:get, head_commit_api_url)
       .to_return(status: 200, body: head_commit_api_response.to_json, headers: { "Content-Type": "application/json" })
   end
+end
+
+def stub_remote_allowlist
+  contents_api_response = <<~EXTERNAL_CONFIG_YAML
+    dependabot_auto_merge_config:
+      api_version: 0 # still feeling out the API. Experimental. This file is subject to change.
+      auto_merge:
+        - dependency: govuk_publishing_components
+          allowed_semver_bumps:
+            - patch
+            - minor
+        - dependency: rubocop-govuk
+          allowed_semver_bumps:
+            - patch
+            - minor
+  EXTERNAL_CONFIG_YAML
+
+  stub_request(:get, external_config_file_api_url)
+    .to_return(status: 200, body: contents_api_response.to_json, headers: { "Content-Type": "application/json" })
 end
