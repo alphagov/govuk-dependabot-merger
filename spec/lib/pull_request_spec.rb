7f18a661e583c0bbb491e73bb4709ddef1c6f80c
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-10-17 10:34:40 +0100

    Allow changes to Gemfile
    Fixes #83
    
    Rails upgrades that have been configured to auto-merge fail because
    they touch the `Gemfile`:
    
    ```
     - Inspecting whitehall#9528...
        ...bad PR: PR changes files that should not be changed. Skipping.
    ```
    
    I would argue that tweaks to the Gemfile are allowable.

--- spec/lib/pull_request_spec.rb
+++ spec/lib/pull_request_spec.rb
@@ -201,6 +201,16 @@ RSpec.describe PullRequest do
       expect(pr.validate_files_changed).to eq(true)
     end
 
+    it "returns true if PR changes Gemfile and Gemfile.lock" do
+      gemfile = head_commit_api_response[:files].first.dup
+      gemfile[:filename] = "Gemfile"
+      head_commit_api_response[:files] << gemfile
+      stub_remote_commit(head_commit_api_response)
+
+      pr = PullRequest.new(pull_request_api_response)
+      expect(pr.validate_files_changed).to eq(true)
+    end
+
     it "returns true if PR only changes yarn.lock" do
       head_commit_api_response[:files][0][:filename] = "yarn.lock"
       stub_remote_commit(head_commit_api_response)
